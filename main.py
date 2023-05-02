e17f2065b125e50dd9cda63ece04c07067ffac63
VarunThejaT <varuntheja.atp@gmail.com>
2023-05-02 02:36:34 -0700

    first change to move to sortino UI

--- main.py
+++ main.py
@@ -42,63 +42,31 @@ def load_LLM(openai_api_key):
     llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
     return llm
 
-st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
-st.header("Globalize Text")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.markdown("")
-
-with col2:
-    st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
-
-st.markdown("## Enter Your Email To Convert")
-
-def get_api_key():
-    input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input")
-    return input_text
-
-openai_api_key = get_api_key()
+st.set_page_config(page_title="Sortino", page_icon=":robot:")
+st.header("Sortino")
 
 col1, col2 = st.columns(2)
 with col1:
-    option_tone = st.selectbox(
-        'Which tone would you like your email to have?',
-        ('Formal', 'Informal'))
+    # choose the company - this should be a pretty quick lookup of all the options available
+    # for demo purposes, we would have just one drop down
+    option_company = st.selectbox( 'Company?', ('Netflix'))
     
 with col2:
-    option_dialect = st.selectbox(
-        'Which English Dialect would you like?',
-        ('American', 'British'))
+    st.markdown('Sources:')
+    option_dialect = st.radio(('News'))
 
 def get_text():
-    input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
+    input_text = st.text_area(label="what is your query?", label_visibility='collapsed', placeholder="Your query...", key="text_input")
     return input_text
 
-email_input = get_text()
+text_input = get_text()
 
-if len(email_input.split(" ")) > 700:
-    st.write("Please enter a shorter email. The maximum length is 700 words.")
+if len(text_input.split(" ")) > 700:
+    st.write("Please enter a shorter query. The maximum length is 700 words.")
     st.stop()
 
-def update_text_with_example():
-    print ("in updated")
-    st.session_state.email_input = "Sally I am starts work at yours monday from dave"
+def fetch_the_relevant_information():
+    pass
+    # here comes the invocation to the berri.ai for the demo purposes
 
-st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
-
-st.markdown("### Your Converted Email:")
-
-if email_input:
-    if not openai_api_key:
-        st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
-        st.stop()
-
-    llm = load_LLM(openai_api_key=openai_api_key)
-
-    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
-
-    formatted_email = llm(prompt_with_email)
-
-    st.write(formatted_email)
+st.button("Send", type='secondary', help="Click to query", on_click=fetch_the_relevant_information)
