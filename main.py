792d857e2572ffa411f6c082db459c0c63a4b501
VarunThejaT <varuntheja.atp@gmail.com>
2023-05-05 00:04:41 -0700

    trying out chat space

--- main.py
+++ main.py
@@ -2,36 +2,48 @@ import streamlit as st
 from langchain import PromptTemplate
 from langchain.llms import OpenAI
 
-st.session_state.option_source_news = False
-
-st.set_page_config(page_title="Sortino", page_icon=":robot:")
-st.header("Sortino")
-
-col1, col2 = st.columns(2)
-with col1:
-    # choose the company - this should be a pretty quick lookup of all the options available
-    # for demo purposes, we would have just one drop down
-    option_company = st.selectbox( 'Company?', ['Netflix'])
-    
-with col2:
-    # choose the source 
-    st.markdown('Source:')
-    # option_source = st.radio('Sources:', ['News'])
-    st.checkbox('News', key="option_source_news")
-    # st.checkbox('News', key="option_source_news")
 
 def get_text():
     input_text = st.text_area(label="what is your query?", label_visibility='collapsed', placeholder="Your query...", key="text_input")
     return input_text
 
-text_input = get_text()
-
-if len(text_input.split(" ")) > 700:
-    st.write("Please enter a shorter query. The maximum length is 700 words.")
-    st.stop()
 
 def fetch_the_relevant_information():
     pass
     # here comes the invocation to the berri.ai for the demo purposes
 
-st.button("Send", type='secondary', help="Click to query", on_click=fetch_the_relevant_information)
+# st.button("Send", type='secondary', help="Click to query", on_click=fetch_the_relevant_information)
+
+
+def main():
+    # Create a persistent state to store the chat history
+    if 'chat_history' not in st.session_state:
+        st.session_state.chat_history = []
+
+    # Input field for the user to enter their message
+    # user_input = st.text_input("Enter your message:")
+
+    text_input = get_text()
+
+    if len(text_input.split(" ")) > 700:
+        st.write("Please enter a shorter query. The maximum length is 700 words.")
+        st.stop()
+        
+    # Button to send the message
+    if st.button("Send", type='secondary', help="Click to query"):
+        # Append the user's message to the chat history
+        st.session_state.chat_history.append(("You", user_input))
+
+        # Simulate a response from the chatbot (you can replace this with your own logic)
+        response = "Hello, I am a chatbot. You said: " + user_input
+        st.session_state.chat_history.append(("Chatbot", response))
+
+        # Clear the input field
+        st.text_input("Enter your message:", value='', key=1)
+
+    # Display the chat history
+    for sender, message in st.session_state.chat_history:
+        st.write(f"{sender}: {message}")
+
+if __name__ == "__main__":
+    main()
